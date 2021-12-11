37b4ee2d49fd2a015bb12792be2431b3e3ea96b6
jbacker2012 <jbacker2012@gmail.com>
2021-12-11 15:36:11 -0600

    12-11-21

---
+++ CraftersCreations/Models/Project.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CraftersCreations.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Catagory Catagory { get; set; }
+    }
+}
